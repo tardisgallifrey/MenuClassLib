ecde727d22ea697e7cb735f9f124ab22e4cae068
Dave <bigmanrunning@gmail.com>
2020-12-23 14:40:01 -0600

    Update 12/23/2020

--- MenuShow.cs
+++ MenuShow.cs
@@ -1,9 +1,13 @@
 using System;
+using MenuClassLib.Models;
 
 namespace MenuClassLib
 {
     public class MenuShow
     {
-    
+        MenuShow(Menu menu)
+        {
+            Console.Clear();    
+        }
     }
 }
